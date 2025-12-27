21e50d5280a6d010ceb3fa20af1889abbf94c05e
sastreexauce01 <sastreexauce01@gmail.com>
2025-12-27 11:48:45 +0100

    first commit

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,24 @@
+import { dashboard, login, register } from '@/routes';
+import { type SharedData } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Welcome({
+    canRegister = true,
+}: {
+    canRegister?: boolean;
+}) {
+    const { auth } = usePage<SharedData>().props;
+
+    return (
+        <>
+            <Head title="Welcome">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link
+                    href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
+                    rel="stylesheet"
+                />
+            </Head>
+         
+        </>
+    );
+}
